b27e27d5f7eb7a87636de11c5d5d2b0e2b125a82
Alexandra Vega <alextvg28@gmail.com>
2023-04-11 23:48:21 -0500

    organizing file into columns and selecting columns

--- parseGFF.py
+++ parseGFF.py
@@ -2,6 +2,8 @@
 
 # import modules
 import argparse
+#import csv
+import csv
 
 # create an ArgumentParser object
 parser = argparse.ArgumentParser(description="This script parses a GFF file")
@@ -12,12 +14,20 @@ parser.add_argument("fasta", help="Name of the FASTA file to parse", type=str)
 
 # parse the actual arguments
 # access argument values via `args` variable
+
 args = parser.parse_args()
 
-# open the GFF file
-with open(args.gff) as x:
+# Open the GFF file
+with open( args.gff) as fgen:
+
 
 	# loop over all the lines in the file
-	for line in x:
-		print(line)
-
+	for line in fgen:
+		line.rstrip()
+		column =line.split('\t')
+		type_fea = column[2]
+		fea_ini = int(column[3])
+		fea_en = int(column[4])
+		fea_len	= fea_en - fea_ini + 1
+		print(type_fea, fea_len)
+	
